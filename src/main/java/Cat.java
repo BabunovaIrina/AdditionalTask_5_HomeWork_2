a0c62d3fb24908fbc73cb3b547a772d4157982ab
irinababunova <xleyla@gmail.com>
2023-10-31 00:16:05 +0300

    Second commit

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -2,33 +2,26 @@ public class Cat {
     private String name; // Поля сделать private
     private int age; // private
     private int weight; // private // Добавила сеттер и геттер для weith
-
     public Cat(String name, int age, int weight) {
         this.name = name;
         this.age = age;
         this.weight = weight;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
-
     public int getWeight() {
         return weight;
     }
-
     public void setWeight(int weight) {
         this.weight = weight;
     }
